f36d8c8bf723121167702ed598bab57f17d6732a
Anton <antonizotov@ukr.net>
2023-01-01 13:49:39 +0200

    test commit3

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -40,7 +40,7 @@ class EmployersAddForm extends Component {
           />
 
           <button type="submit" onClick={(e) => addItem(e, name, salary)} className="btn btn-outline-light">
-             Добавитьfff
+             Добавить
           </button>
         </form>
       </div>
